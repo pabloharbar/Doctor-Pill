9315a2757042279ef64d95872a7102564f9048bb
Pablo <harbar2705@gmail.com>
2021-09-02 16:24:04 -0300

    Project initialize

---
+++ Doctor Pill/Doctor Pill/Doctor_PillApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctor_PillApp.swift
+//  Doctor Pill
+//
+//  Created by Pablo Penas on 02/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Doctor_PillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
