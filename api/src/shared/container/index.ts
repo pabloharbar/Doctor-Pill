5d662fab04dd8016b3da1103a991cf3aaefa1a20
Thiago Henrique Xavier Medeiros <thihxm@gmail.com>
2021-09-08 18:48:36 -0300

    Register reference drugs repo

--- api/src/shared/container/index.ts
+++ api/src/shared/container/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe'
 
 import { DrugsRepository } from '@modules/drugs/infra/typeorm/repositories/DrugsRepository'
+import { ReferenceDrugsRepository } from '@modules/drugs/infra/typeorm/repositories/ReferenceDrugsRepository'
 import { IDrugsRepository } from '@modules/drugs/repositories/IDrugsRepository'
+import { IReferenceDrugsRepository } from '@modules/drugs/repositories/IReferenceDrugsRepository'
 
 container.registerSingleton<IDrugsRepository>(
   'DrugsRepository',
   DrugsRepository
+)
+
+container.registerSingleton<IReferenceDrugsRepository>(
+  'ReferenceDrugsRepository',
+  ReferenceDrugsRepository
 )
