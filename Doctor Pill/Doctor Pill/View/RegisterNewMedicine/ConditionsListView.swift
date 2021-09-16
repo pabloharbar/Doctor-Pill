fe8a20a545b8fe34d81414ebc19185ab749c2c17
Pablo <harbar2705@gmail.com>
2021-09-16 15:29:10 -0300

    Enhanced register view

---
+++ Doctor Pill/Doctor Pill/View/RegisterNewMedicine/ConditionsListView.swift
@@ -0,0 +1,20 @@
+//
+//  ConditionsListView.swift
+//  Doctor Pill
+//
+//  Created by Pablo Penas on 16/09/21.
+//
+
+import SwiftUI
+
+struct ConditionsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConditionsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConditionsListView()
+    }
+}
---
+++ Doctor Pill/Doctor Pill/View/RegisterNewMedicine/ListInputView.swift
@@ -0,0 +1,20 @@
+//
+//  ListInputView.swift
+//  Doctor Pill
+//
+//  Created by Pablo Penas on 16/09/21.
+//
+
+import SwiftUI
+
+struct ListInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListInputView()
+    }
+}
